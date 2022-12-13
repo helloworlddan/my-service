a4d67437f7464dff937c28e52997ca306fdbbc58
Daniel Stamer <stamer@google.com>
2022-12-13 10:42:18 +0000

    initial
    Signed-off-by: Daniel Stamer <stamer@google.com>

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/helloworlddan/tortune"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, tortune.HitMe())
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
